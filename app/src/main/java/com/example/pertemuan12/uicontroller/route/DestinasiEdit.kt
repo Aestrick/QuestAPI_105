35fd7450ba1147954247b62a9bf10c793c5158e5
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-12-18 20:05:07 +0700

    mengubah beberapa kode file

--- app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/pertemuan12/uicontroller/route/DestinasiEdit.kt
@@ -5,7 +5,6 @@ import com.example.pertemuan12.R
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "edit"
     override val titleRes = R.string.edit_siswa
-    // GANTI 'nim' JADI 'nama'
     const val nama = "nama"
     val routeWithArgs = "$route/{$nama}"
 }
