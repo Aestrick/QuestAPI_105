559d77133bf0d67351558a4fa1dd8dff099508e5
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-12-25 14:27:39 +0700

    mengubah beberapa kode file

--- app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
@@ -4,7 +4,41 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class DataSiswa(
+    val id: Int,
     val nama: String,
     val alamat: String,
     val telpon: String
+)
+
+// --- TAMBAHKAN KODE DI BAWAH INI ---
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun DataSiswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
 )
